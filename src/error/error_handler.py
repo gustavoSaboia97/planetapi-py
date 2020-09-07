f6cd1f2f09fe947327d9cf8734177e6858e4fd1d
Gustavo Saboia <gustavosaboia97@hotmail.com>
2020-09-07 20:05:50 -0300

    Ajustes de tratamento de erro

--- src/error/error_handler.py
+++ src/error/error_handler.py
@@ -1,16 +1,17 @@
 from .api_error import APIError
 from src.util.logger import get_logger
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import JSONResponse
+from fastapi import Response, status
 
 
 logger = get_logger()
 
 
-def api_errors(exception: APIError) -> PlainTextResponse:
+def api_errors(exception: APIError) -> Response:
     logger.error(f"[API ERRORS] An error occurred during api processing {exception.message}")
-    return PlainTextResponse(str(exception), status_code=exception.status_code)
+    return JSONResponse(content=exception.to_dict(), status_code=exception.status_code)
 
 
-def general_errors(exception: Exception) -> PlainTextResponse:
+def general_errors(exception: Exception) -> Response:
     logger.error(f"[GENERAL ERRORS] An error was found on API {str(exception)}")
-    return PlainTextResponse(str(""), status_code=500)
+    return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from src.error.api_error import APIError
 from src.error.error_handler import api_errors, general_errors
 
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.exception_handler(APIError)
-def deal_with_api_errors(request, error: APIError):
+def deal_with_api_errors(request, error: APIError) -> Response:
     return api_errors(error)
 
 
 @app.exception_handler(Exception)
-def deal_with_general_errors(request, error: Exception):
+def deal_with_general_errors(request, error: Exception) -> Response:
     return general_errors(error)
